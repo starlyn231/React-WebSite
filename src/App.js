787a5c13a9b7df47c248be488bc772fb61b5b4d7
starlyn231 <starlynfernandez1222@hotmail.com>
2021-08-28 19:44:05 -0400

    ADD COMPONENT OF STYLE

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>
       <Router>
         <Navbar />
         <Switch>
